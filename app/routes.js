2829527560991483663c1a7e1518ccbf32353d57
Dominic Norris <you@example.com>
2020-11-10 12:31:23 +0000

    Added checkbox branching and new route without wcag questions

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,19 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.use('/01', require('./views/01/index'));
+router.post('/participants', function (req, res) {
+
+  let inputName1 = req.session.data['participants']
+  let inputName2 = req.session.data['participants'] 
+  if (inputName1 === 'usability' ) {
+    res.redirect('/01/wcag')
+  } else if (inputName2 === 'other') {
+    res.redirect('/01/no-wcag-route/what-phase')
+  } else {
+    res.redirect('/01/wcag')
+  }
+
+})
+
 
 module.exports = router
